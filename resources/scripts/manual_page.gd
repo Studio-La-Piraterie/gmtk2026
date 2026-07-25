5bc0cbcbb94214fa44428657e2b7f87cc97059c8
Thibault/Bakou <t.bardecab@gmail.com>
2026-07-25 13:16:07 +0200

    User Manual v1.0
    - Tourner les pages
    - Ajout de pages via resource
    Fonctionnel :)
    TODO -> Mode Zoom (Est-ce que le manuel est ouvert uniquement en ZOOM et
    fermé quand posé sur le tableua de bord ? )

--- resources/scripts/manual_page.gd
+++ resources/scripts/manual_page.gd
@@ -1,4 +1,4 @@
-class_name ManualPage extends Node
+class_name ManualPage extends Resource
 
 @export var text : String
 @export var type : PageType = PageType.PAGE
@@ -11,24 +11,16 @@ class_name ManualPage extends Node
 enum PageType {PAGE, BEGINNING_FRONT_COVER, BEGINNING_BACK_COVER, ENDING_FRONT_COVER, ENDING_BACK_COVER}
 
 const PAGE_SPRITE : Dictionary = {
-	PageType.PAGE : {
-		#Sprite
-	},
-	PageType.BEGINNING_FRONT_COVER: {
-		#Sprite
-	},
-	PageType.BEGINNING_BACK_COVER: {
-		#Sprite
-	},
-	PageType.ENDING_FRONT_COVER: {
-		#Sprite
-	},
-	PageType.ENDING_BACK_COVER: {
-		#Sprite
-	}
+	PageType.PAGE : preload("res://assets/images/placeholders/blankpage.png"),
+	PageType.BEGINNING_FRONT_COVER: preload("res://assets/images/placeholders/front.png"),
+	PageType.BEGINNING_BACK_COVER: null,
+	PageType.ENDING_FRONT_COVER: preload("res://assets/images/placeholders/back.png"),
+	PageType.ENDING_BACK_COVER: null
 }
 
-@onready var sprite : Texture2D = PAGE_SPRITE[type]
+var sprite : Texture2D:
+	get:
+		return PAGE_SPRITE.get(type, null)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
--- scenes/main/scripts/UserManual/testUserManual.gd
+++ scenes/main/scripts/UserManual/testUserManual.gd
@@ -3,7 +3,12 @@ class_name  UserManual extends Node
 @export var pages_arr : Array[ManualPage]
 
 @onready var left_page = $PagesContainer/LeftPage
-@onready var right_page = $PagesContainer/RighPage
+@onready var left_page_text = $PagesContainer/LeftPage/LeftPageText
+@onready var left_page_button = $PagesContainer/LeftPage/LeftPageButton
+
+@onready var right_page = $PagesContainer/RightPage
+@onready var right_page_text = $PagesContainer/RightPage/RightPageText
+@onready var right_page_button = $PagesContainer/RightPage/RightPageButton
 
 # -1 signifie aucune page à afficher | 
 # La première page commence à l'index 0 (couverture)
@@ -13,44 +18,54 @@ var current_right_page : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	left_page_button.pressed.connect(previous_page)
+	right_page_button.pressed.connect(next_page)
+	set_left_page()
+	set_right_page()
+	if pages_arr.size() % 2 != 0:
+		push_error("The number of pages of the user manual is odd, risks of bug")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-## On tourne la page, sauf si la dernière page est déjà atteinte
 func next_page() -> void:
 	# Si la page de droite ne contient aucune page, on annule
 	if current_right_page == -1:
 		return
 	# Si on est à la dernière page à droite, on enlève la page de droite
-	if current_right_page + 1 == pages_arr.size():
+	if current_right_page + 2 >= pages_arr.size():
 		current_right_page = -1
 	else:
-		current_right_page += 1
-	current_left_page += 1
+		current_right_page += 2
+	current_left_page += 2
+	set_left_page()
+	set_right_page()
 
 func previous_page() -> void:
 	# Si la page de gauche ne contient aucune page, on annule
-	if (current_left_page == -1):
+	if current_left_page == -1:
 		return
-	# Si on est à la première page à gauche, on enlève la page de gauche
-	if current_left_page == 0:
-		current_left_page = -1
+	if current_right_page == -1:
+		current_right_page = pages_arr.size() - 2
 	else:
-		current_left_page -= 1
-	current_right_page -= 1
+		current_right_page -= 2
+	current_left_page -= 2
+	set_left_page()
+	set_right_page()
 	
 func set_right_page() -> void:
+	right_page_text.clear()
 	if current_right_page == -1:
 		right_page.texture = null
 	elif current_right_page + 1 <= pages_arr.size():
 		right_page.texture = pages_arr[current_right_page].sprite
+		right_page_text.add_text(pages_arr[current_right_page].text) 
 
 func set_left_page() -> void:
+	left_page_text.clear()
 	if current_left_page == -1:
 		left_page.texture = null
 	elif current_left_page + 1 <= pages_arr.size():
 		left_page.texture = pages_arr[current_left_page].sprite
+		left_page_text.add_text(pages_arr[current_left_page].text)
