1fd88390778b6ad7685bf539d8abbb338fd83f17
Sharky_BSA <sharky.tf@gmail.com>
2026-07-26 00:52:23 +0200

    Sharky - sequence de tir

--- scenes/dashboard_ui/resolvedismiss/scripts/resolve_dismiss_ui.gd
+++ scenes/dashboard_ui/resolvedismiss/scripts/resolve_dismiss_ui.gd
@@ -17,6 +17,7 @@ var resolve_machine_state : ResolveMachineState = ResolveMachineState.NONE :
 	set = set_state
 
 var disabled := false
+var diode_turn_on_wait_time : float = 1.2
 
 enum ResolveMachineState {
 	NONE = -255,
@@ -40,30 +41,53 @@ func _ready():
 	set_state(ResolveMachineState.NO_ENCOUNTER)
 	
 func set_state(new_state : ResolveMachineState) ->void:
-	if new_state == ResolveMachineState.OK_PRESSED && resolve_machine_state>new_state:
-		return
-	if new_state == ResolveMachineState.CTRL_PRESSED && resolve_machine_state>new_state:
-		return
-	if new_state == ResolveMachineState.READY_PRESSED && resolve_machine_state>new_state:
-		return
-	if new_state == ResolveMachineState.ENGAGE_SWITCHED && resolve_machine_state>=new_state:
-		new_state = ResolveMachineState.ENCOUNTER_PRESENT
+	
+	match new_state:
+		ResolveMachineState.ENGAGE_SWITCHED:
+			if resolve_machine_state>=new_state:
+				resolve_machine_state = ResolveMachineState.ENCOUNTER_PRESENT
+			else :
+				resolve_machine_state = new_state
+				await get_tree().create_timer(diode_turn_on_wait_time).timeout
+
+		ResolveMachineState.OK_PRESSED:
+			if resolve_machine_state>new_state:
+				return
+			if resolve_machine_state == ResolveMachineState.ENGAGE_SWITCHED:
+				resolve_machine_state = new_state
+				await get_tree().create_timer(diode_turn_on_wait_time).timeout
 		
-	resolve_machine_state = new_state
+		ResolveMachineState.CTRL_PRESSED:
+			if resolve_machine_state>new_state:
+				return
+			if resolve_machine_state == ResolveMachineState.OK_PRESSED:
+				resolve_machine_state = new_state
+				await get_tree().create_timer(diode_turn_on_wait_time).timeout
+		
+		ResolveMachineState.READY_PRESSED:
+			if resolve_machine_state>new_state:
+				return
+			if resolve_machine_state == ResolveMachineState.CTRL_PRESSED:
+				resolve_machine_state = new_state
+				await get_tree().create_timer(diode_turn_on_wait_time).timeout
+		_:
+			resolve_machine_state = new_state
+
 	if not disabled:
 		_update_resolve_machine_ui(resolve_machine_state)
 
 func _update_resolve_machine_ui(new_state : ResolveMachineState)->void:
 	if new_state == ResolveMachineState.NONE: return
+	
 	engage_btn.disabled = new_state < ResolveMachineState.ENCOUNTER_PRESENT
 	ok_btn.disabled = new_state < ResolveMachineState.ENGAGE_SWITCHED
-	ok_diode.color = Color.RED if new_state < ResolveMachineState.OK_PRESSED else Color.GREEN  
+	ok_diode.color = Color.RED if new_state < ResolveMachineState.ENGAGE_SWITCHED else Color.GREEN  
 	ctrl_btn.disabled = new_state < ResolveMachineState.OK_PRESSED
-	ctrl_diode.color = Color.RED if new_state < ResolveMachineState.CTRL_PRESSED else Color.GREEN  
+	ctrl_diode.color = Color.RED if new_state < ResolveMachineState.OK_PRESSED else Color.GREEN  
 	ready_btn.disabled = new_state < ResolveMachineState.CTRL_PRESSED
-	ready_diode.color = Color.RED if new_state < ResolveMachineState.READY_PRESSED else Color.GREEN  
+	ready_diode.color = Color.RED if new_state < ResolveMachineState.CTRL_PRESSED else Color.GREEN  
 	kill_btn.disabled = new_state < ResolveMachineState.READY_PRESSED
-	dismiss_btn.disabled = new_state < ResolveMachineState.ENCOUNTER_PRESENT
+	dismiss_btn.disabled = new_state < ResolveMachineState.ENGAGE_SWITCHED
 
 func disable() -> void:
 	disabled = true
